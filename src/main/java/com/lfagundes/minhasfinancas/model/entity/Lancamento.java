93c8771dbb57283e5a4b550083a3de1847588770
Lucas Fagundes <lucas@lucasfagundesti.com>
2021-06-08 00:52:31 -0300

    Second commit

--- src/main/java/com/lfagundes/minhasfinancas/model/entity/Lancamento.java
+++ src/main/java/com/lfagundes/minhasfinancas/model/entity/Lancamento.java
@@ -6,6 +6,8 @@ import java.time.LocalDate;
 import javax.persistence.Column;
 import javax.persistence.Convert;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -15,14 +17,20 @@ import javax.persistence.Table;
 
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
+import lombok.Data;
+
 @Entity
 @Table(name="lancamento", schema = "financas")
+@Data
 public class Lancamento {
 
 	@Id
 	@Column
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(name = "descricao")
+	private String descricao;
 	
 	@Column(name="mes")
 	private Integer mes;
@@ -40,4 +48,14 @@ public class Lancamento {
 	@Column(name = "data_cadastro")
 	@Convert(converter = Jsr310JpaConverters.LocalDateConverter.class)
 	private LocalDate dataCadastro;
+	
+	@Column(name="tipo")
+	@Enumerated(value = EnumType.STRING)
+	private TipoLancamento tipo;
+	
+	@Column(name = "status")
+	@Enumerated(value=EnumType.STRING)
+	private StatusLancamento status;
+	
+	
 }
--- src/main/java/com/lfagundes/minhasfinancas/model/entity/Usuario.java
+++ src/main/java/com/lfagundes/minhasfinancas/model/entity/Usuario.java
@@ -7,53 +7,33 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Data;
+
 @Entity
-@Table(name="usuario", schema="financas")
+@Table(name = "usuario", schema = "financas")
+@Data
 public class Usuario {
 
 	@Id
-	@Column(name="id")
+	@Column(name = "id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
-	@Column(name="nome")
+
+	@Column(name = "nome")
 	private String nome;
-	
-	@Column(name="email")
+
+	@Column(name = "email")
 	private String email;
-	
-	@Column(name="senha")
+
+	@Column(name = "senha")
 	private String senha;
 
-	public Long getId() {
-		return id;
+	public static void main(String[] args) {
+		Usuario usuario = new Usuario();
+		
+		usuario.setEmail("teste@teste");
+		usuario.setSenha("123");
+		usuario.setNome("nome");
 	}
 
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
 }
