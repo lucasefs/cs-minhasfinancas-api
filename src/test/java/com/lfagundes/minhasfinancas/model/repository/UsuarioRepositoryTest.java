b656442b67e26857734af565e36682d2a3b903d4
Lucas Fagundes <lucas@lucasfagundesti.com>
2021-06-10 00:59:01 -0300

    Fourth commit

--- src/test/java/com/lfagundes/minhasfinancas/model/repository/UsuarioRepositoryTest.java
+++ src/test/java/com/lfagundes/minhasfinancas/model/repository/UsuarioRepositoryTest.java
@@ -4,24 +4,31 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import com.lfagundes.minhasfinancas.model.entity.Usuario;
 
-@SpringBootTest
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class UsuarioRepositoryTest {
 
 	@Autowired
 	UsuarioRepository repository;
-	
+
+	@Autowired
+	TestEntityManager entityManager;
+
 	@Test
-	public void deveVerificarAExistenciaDeUmEmail() {
+	public void deveVerificarAExistenciaDeUmEmail()
+	{
 		
 		Usuario usuario = Usuario.builder().nome("usuario").email("usuario@email.com").build();
-		repository.save(usuario);
+		entityManager.persist(usuario);
 		
 		boolean result = repository.existsByEmail("usuario@email.com");
 		
@@ -29,10 +36,9 @@ public class UsuarioRepositoryTest {
 	}
 	
 	@Test
-	public void deveRetornarFalsoQuandoNaoHouverUsuarioCadastrado() {
-		
-		repository.deleteAll();
-		
+	public void deveRetornarFalsoQuandoNaoHouverUsuarioCadastrado()
+	{
+
 		boolean result = repository.existsByEmail("usuario@2email.com");
 		
 		Assertions.assertThat(result).isFalse();
